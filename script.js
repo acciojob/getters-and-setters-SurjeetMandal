c832fa45cdaebad9c43ed79ccd4c9d9f904bb303
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-02 18:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//complete this code
 class Person {
-	constructor(name,age){
+	constructor(name, age){
 		this.Name = name,
 		this.Age = age
 	}
@@ -8,22 +7,22 @@ class Person {
 		return this.Name;
 	}
 	set age(value){
-		if(typeof age !== 'number' || value < 0){
-			throw new Error('Age must be positive number.');
+		if(typeof value !== 'number' || value < 0){
+			throw new Error('Age must be a positive number.');
 		}
-		this.age = value;
+		this.Age = value;
 	}
 }
 
 class Student extends Person {
 	study(name){
-		console.log(name+"is studying");
+		console.log(name + " is studying");
 	}
 }
 
 class Teacher extends Person {
 	teach(name){
-		console.log(name+"is teaching")
+		console.log(name + " is teaching");
 	}
 }
 
