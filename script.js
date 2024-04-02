5f6332f4e444d3efe2aa7060ff1c810e0406bd96
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-02 17:53:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = Name,
+		this.age = Age
+	}
+	get Name(){
+		return this.name;
+	}
+	set Age(age){
+		if(typeof age !== 'number'){
+			throw new Error('Age must be number.');
+		}
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(name){
+		console.log(name+"is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(name){
+		console.log(name+"is teaching")
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
